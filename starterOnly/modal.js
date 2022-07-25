c0ee9deef3d40819b8bb637c446837e7cc5b1a08
Jean-Baptiste Guerin <jb.l.guerin@gmail.com>
2022-07-25 17:27:41 +0200

    first-day 25/07/22

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,19 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalclose = document.querySelector(".close")
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
+modalclose.addEventListener("click", exitModal);
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+//exit modal event 
+function exitModal() {
+  modalbg.style.display = "none";
 }
 
 
